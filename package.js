3dd1bf682891c88c22d9972477b9995aad9f1759
Ross Waycaster <rosswaycaster@me.com>
2015-10-29 11:31:45 -0500

    Updates package information.

--- package.js
+++ package.js
@@ -1,24 +1,38 @@
 Package.describe({
   name: 'rosswaycaster:meteor-accounts-wordpress',
-  version: '0.0.1',
+  version: '0.1.0',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Meteor login service for self-hosted WordPress accounts with a little more customization',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/rosswaycaster/Meteor-Accounts-Wordpress',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.2.1');
-  api.use('ecmascript');
-  api.addFiles('meteor-accounts-wordpress.js');
-});
+Package.on_use(function(api) {
+    api.versionsFrom('METEOR@0.9.4');
+    api.use('accounts-base', [ 'client', 'server' ]);
+    api.imply('accounts-base', [ 'client', 'server' ]);
+    api.use('accounts-oauth', [ 'client', 'server' ]);
+    api.imply('accounts-oauth', [ 'client', 'server' ]);
 
-Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('rosswaycaster:meteor-accounts-wordpress');
-  api.addFiles('meteor-accounts-wordpress-tests.js');
+    api.use('oauth', [ 'client', 'server' ]);
+    api.use('oauth2', [ 'client', 'server' ]);
+    api.use('http', [ 'server' ]);
+    api.use('templating', 'client');
+    api.use('underscore', 'server');
+    api.use('random', 'client');
+    api.use('service-configuration', [ 'client', 'server' ]);
+
+    api.addFiles('wordpress_client.js', 'client');
+    api.addFiles('wordpress_server.js', 'server');
+    api.addFiles("wordpress.js");
+
+    api.export('WordPress');
+
+    api.addFiles([
+        'wordpress_configuration.html',
+        'wordpress_configuration.js'
+    ], 'client');
 });
